4133c4613bbe09ba0a0b5a04165bd6624ffb53ae
Andy Willis <awillis@fastmail.fm>
2023-04-21 21:58:02 +0100

    Updated

--- sign.sh
+++
@@ -1,1 +0,0 @@
-web-ext sign --api-key user:13200538:167 --api-secret f8802017aa8bad35de60ec7e43654810d2c1816864ae6a5fc5f215ee6e26710e --ignore-files package.json package-lock.json README.md rundev.sh build.sh sign.sh
