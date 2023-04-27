1e8bf4bdb30f63724f68a62570cb55fa686c2cd4
Andy Willis <andywillis@users.noreply.github.com>
2023-04-27 23:28:03 +0100

    Updated React (#7)
    Co-authored-by: Andy Willis <awillis@fastmail.fm>

--- rundev.sh
+++ rundev.sh
@@ -1,1 +1,3 @@
+#! /usr/bin/bash
+
 web-ext run --firefox="/c/Program Files/Firefox Developer Edition/firefox.exe" --firefox-profile="/c/Users/accou/AppData/Roaming/Mozilla/Firefox/Profiles/kvcr4yus.Development"
