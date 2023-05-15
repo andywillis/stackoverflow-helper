db3533abe03a3d93ac96eb81229bf805da5c99d0
Andy Willis <awillis@fastmail.fm>
2023-05-15 19:49:38 +0100

    Updated configuration options

--- options/options.js
+++ options/options.js
@@ -1,7 +1,32 @@
 (async function () {
 
+  const configurationFilename = 'stackoverflow-helper.config';
+
   const buttons = document.querySelector('.buttons');
   const message = document.querySelector('.message');
+
+  message.addEventListener('click', handleYesNo);
+
+  const yesNo = `
+    Are you sure?
+    <button data-check="yes" class="check yes">Yes</button>
+    <button data-check="no" class="check no">No</button>
+  `;
+
+  function handleYesNo(e) {
+    if (e.target.matches('.check')) {
+      if (e.target.dataset.check === 'yes') {
+        resetConfig();
+        message.classList.remove('error');
+        message.classList.add('ok');
+        message.textContent = 'Configuration reset';
+      } else {
+        message.classList.remove('error');
+        message.classList.add('ok');
+        message.textContent = 'Reset cancelled';
+      }
+    }
+  }
 
   function pad(str) {
     return str.toString().length === 1 ? `0${str}` : str;
@@ -23,12 +48,14 @@
     const urlObj = URL.createObjectURL(blob);
     try {
       const id = await browser.downloads.download({
-        filename: `SOhelper-${formatDate()}.json`,
+        filename: `${configurationFilename}-${formatDate()}.json`,
         url: urlObj,
         saveAs: true,
         conflictAction: 'overwrite'
       });
       browser.downloads.erase({ id });
+      message.classList.remove('error');
+      message.classList.add('ok');
       message.textContent = 'Configuration exported';
     } catch (err) {
       console.error(err);
@@ -36,11 +63,19 @@
   }
 
   async function handleLoadConfig(e) {
-    const { result: data } = e.target;
-    const config = JSON.parse(data);
-    await browser.storage.local.set({ config });
-    message.textContent = 'Configuration imported';
-    browser.runtime.sendMessage({ action: 'rebuildMenu' });
+    try {
+      const { result: data } = e.target;
+      const config = JSON.parse(data);
+      await browser.storage.local.set({ config });
+      message.classList.remove('error');
+      message.classList.add('ok');
+      message.textContent = 'Configuration imported';
+      browser.runtime.sendMessage({ action: 'rebuildMenu' });
+    } catch (err) {
+      message.classList.remove('ok');
+      message.classList.add('error');
+      message.textContent = 'Error importing configuration - file invalid';
+    }
   }
 
   function handleChange(e) {
@@ -60,10 +95,23 @@
     input.addEventListener('change', handleChange, false);
   }
 
+  async function resetConfig() {
+    await browser.storage.local.set({ config });
+    message.classList.remove('error');
+    message.classList.add('ok');
+    message.textContent = 'Configuration reset';
+    browser.runtime.sendMessage({ action: 'rebuildMenu' });
+  }
+
+  function handleReset() {
+    message.innerHTML = yesNo;
+  }
+
   function handleClick(e) {
     const { dataset: { action } } = e.target;
     if (action === 'import') handleImport();
     if (action === 'export') handleExport();
+    if (action === 'reset') handleReset();
   }
 
   buttons.addEventListener('click', handleClick, false);
